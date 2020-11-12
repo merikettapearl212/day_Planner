c8ce140c8fdf01c5390fc03cb18d2acf7e3d67e1
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-11-12 01:00:19 -0800

    working hours

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,4 @@
-var hourEl;
-var timeBlock;
 
-var timeBlockEl = $(".time-block");
 // adding moment js for today date in the header
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
@@ -10,25 +7,20 @@ $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 var currentHour = parseInt(today.format("H"));
 
 // timeBlock
-workingHours = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"];
+var workingHours = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"];
 //make a function...
 // CREATE time blocks from 8AM to 6PM
-function createBlocks() {
-    for (i = 0; i < workingHours.length; i++) {
-        timeBlock = $("<div>").addClass("row");
-        timeBlockEl.append(timeBlock);
-        hourEl = $("<div>").addClass("hour col text-right").text(workingHours[i]);
-    //add class + attr
-    //append to html elements
-// ADD save icon
-// Set data attr's
-}
-}
 
+        //add class + attr //append to html elements
+     
+// ADD save icon
+     
 //make a function that...
 // CHECK that each timeblock is color coded to indicate whether it is in the past-grey, present-orange, or future-green
-    //for loop
-    //add class
+
+    //grab button id and value and send it to local storage
+
+    //call function to save data to local storage
 
 //make a funtion...
 //GET and SAVE after btn 'click'
@@ -38,6 +30,5 @@ function createBlocks() {
 
 //CALL functions
 
-createBlocks()
 
 
