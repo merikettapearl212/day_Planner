0dedb3c71715d48d380eb335c921164edcb65aca
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-11-11 17:50:31 -0800

    adding todays date in header

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+
+// ADD todays date w/ moment.js in header 
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
+// GET current hour w/ moment.js
+
+//
+// PRESENT timeblocks for standard business hours for 8am - 6pm
+/* CHECK timeblock is color coded to indicate whether it is in the past, present, or future*/
+
+// Save value to local storage on click
+// Get values from local storage
