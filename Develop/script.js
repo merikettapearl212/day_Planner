a1011fb1a4ba4b9ef0f080bc6a76eafe9209dc3a
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-11-11 23:04:43 -0800

    working on it

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,26 @@
 
-// ADD todays date w/ moment.js in header 
+// adding moment js for today date in the header
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
-// GET current hour w/ moment.js
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
-//
-// PRESENT timeblocks for standard business hours for 8am - 6pm
-/* CHECK timeblock is color coded to indicate whether it is in the past, present, or future*/
+// Get the current hour of the day using moment.js
+var currentHour = parseInt(today.format("H"));
 
-// Save value to local storage on click
-// Get values from local storage
+// timeBlock
+
+// Create time blocks for from 9AM to 5PM
+
+        // Add save icon to save buttons
+
+ //check for the bussinessTime match with current time. If past current time, turn grey. if current, turn orange. If future, turn green.
+
+// function to get and save values after saveBtn is clicked.
+
+//set data to JSON
+
+// save data to localStorage
+
+// render data by loop though th array to 
+
+// run the functions
+
