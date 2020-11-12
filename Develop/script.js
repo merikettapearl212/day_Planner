6c81459a0996aeadcc45b61484b7ac1cd1282624
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-11-11 23:56:21 -0800

    colums not showing

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,43 @@
+var hourEl;
+var timeBlock;
 
+var timeBlockEl = $(".time-block");
 // adding moment js for today date in the header
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
 // Get the current hour of the day using moment.js
 var currentHour = parseInt(today.format("H"));
 
 // timeBlock
+workingHours = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"];
+//make a function...
+// CREATE time blocks from 8AM to 6PM
+function createBlocks() {
+    for (i = 0; i < workingHours.length; i++) {
+        timeBlock = $("<div>").addClass("row");
+        timeBlockEl.append(timeBlock);
+        hourEl = $("<div>").addClass("hour col text-right").text(workingHours[i]);
+    //add class + attr
+    //append to html elements
+// ADD save icon
+// Set data attr's
+}
+}
 
-// Create time blocks for from 9AM to 5PM
+//make a function that...
+// CHECK that each timeblock is color coded to indicate whether it is in the past-grey, present-orange, or future-green
+    //for loop
+    //add class
 
-        // Add save icon to save buttons
+//make a funtion...
+//GET and SAVE after btn 'click'
 
- //check for the bussinessTime match with current time. If past current time, turn grey. if current, turn orange. If future, turn green.
+//CALL function that saves to local storage?
+//RENDER data?
 
-// function to get and save values after saveBtn is clicked.
+//CALL functions
 
-//set data to JSON
+createBlocks()
 
-// save data to localStorage
-
-// render data by loop though th array to 
-
-// run the functions
 
